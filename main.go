b4b0b98642f97f219081c42369b0dcc30f0a7a4e
adi kanishka <adikanishka@adis-MacBook-Air.local>
2026-02-09 18:18:49 +0530

    Added get request

--- main.go
+++ main.go
@@ -1,15 +1,42 @@
 package main
 
-import "fmt"
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"log"
+)
+
+type Post struct{
+	ID string `json:"id"`
+	Title string `json:"title"`
+	Content string `json:"content"`
+	Author string `json:"author"`
+}
 
 func handler(w http.ResponseWriter, r *http.Request){
-	fmt.Println("Runing...")
+	fmt.Fprintln(w,"Runing...")
 }
+
+func getHandler(w http.ResponseWriter, r *http.Request){
+	data,err := os.ReadFile("blog.json")
+	if err!=nil{
+		http.Error(w,"Failed",http.StatusInternalServerError)
+		return
+	}
+	
+	var posts []Post
+	json.Unmarshal(data, &posts)
+	w.Header().Set("Content-Type","application/json")
+	json.NewEncoder(w).Encode(posts)
+}
+
 
 func main(){
 	http.HandleFunc("/",handler)
+	http.HandleFunc("/get",getHandler)
 	fmt.Println("Server started....")
-	http.ListenAndServe(":8080",nil)
+	log.Fatal(http.ListenAndServe(":8080",nil))
 
 }
