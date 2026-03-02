8a209a13f0f112dd6a94c353bd6af31b32a5ff99
adi kanishka <adikanishka06@gmail.com>
2026-03-02 12:25:09 +0530

    Added getById Method

--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
-	"log"
+	"strings"
 )
 
 type Post struct{
@@ -32,10 +33,32 @@ func getHandler(w http.ResponseWriter, r *http.Request){
 	json.NewEncoder(w).Encode(posts)
 }
 
+func getByIdHandler(w http.ResponseWriter, r *http.Request){
+	parts:=strings.Split(r.URL.Path,"/")
+	id:=parts[2]
+	data,err :=os.ReadFile("blog.json")
+	if err!=nil{
+		http.Error(w,"Failed",http.StatusInternalServerError)
+		return
+	}
+	var posts []Post
+	json.Unmarshal(data,&posts)
+	for _,p := range posts{
+		if p.ID == id{
+			w.Header().Set("Content-Tyoe","application/json")
+			json.NewEncoder(w).Encode(p)
+			return
+		}
+	}
+	http.Error(w, "Post not found", http.StatusNotFound)
+
+}
+
 
 func main(){
 	http.HandleFunc("/",handler)
 	http.HandleFunc("/get",getHandler)
+	http.HandleFunc("/get/",getByIdHandler)
 	fmt.Println("Server started....")
 	log.Fatal(http.ListenAndServe(":8080",nil))
 
